0563c267be78fdb48be184bc9c708e53d7e096c9
Paresh <pareshnair2411@gmail.com>
2024-09-28 22:37:48 +0530

    third-commit

--- game.js
+++ game.js
@@ -1,37 +1,52 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
-let started = false;
+let gameStarted = false;
 let level = 0;
 
+//Keypress to check if the game started or not
 $(document).keypress(function () {
-  if (!started) {
+  if (!gameStarted) {
     $("#level-title").text(`Level ${level}`);
     nextSequence();
-    started = true;
+    gameStarted = true;
   }
 });
 
+//Click responses
+$(".btn").click(function () {
+  let userChosenColor = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColor);
+  // console.log(userClickedPattern);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+//Main driving function of the game
 function nextSequence() {
   userClickedPattern = [];
   level++;
-
   $("#level-title").text(`Level ${level}`);
-  let randomNumber = Math.floor(Math.random() * 3);
+
+  let randomNumber = Math.floor(Math.random() * 4);
+
   let randomColorChosen = buttonColors[randomNumber];
-
+  // console.log(randomColorChosen);
   gamePattern.push(randomColorChosen);
-
   $(`#${randomColorChosen}`).fadeIn(100).fadeOut(100).fadeIn(100);
-
   playSound(randomColorChosen);
 }
 
+//Adds sound to the button clicks
 function playSound(name) {
   var audio = new Audio(`sounds/${name}.mp3`);
   audio.play();
 }
 
+//Adds animation to the buttons
 function animatePress(currentColor) {
   $(`#${currentColor}`).addClass("pressed");
 
@@ -40,20 +55,10 @@ function animatePress(currentColor) {
   }, 100);
 }
 
-$(".btn").click(function () {
-  let userChosenColor = $(this).attr("id");
-
-  userClickedPattern.push(userChosenColor);
-
-  playSound(userChosenColor);
-  animatePress(userChosenColor);
-
-  checkAnswer(userClickedPattern.length - 1);
-});
-
+//Checks whether the user entered pattern and game's pattern matches
 function checkAnswer(currentLevel) {
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-    console.log("success");
+    // console.log("yes");
 
     if (gamePattern.length === userClickedPattern.length) {
       setTimeout(() => {
@@ -61,23 +66,21 @@ function checkAnswer(currentLevel) {
       }, 1000);
     }
   } else {
-    console.log("failed");
-
     playSound("wrong");
-
     $("body").addClass("game-over");
+
     setTimeout(() => {
       $("body").removeClass("game-over");
     }, 200);
 
-    $("#level-title").text("Game over, Press any key to continue");
-
+    $("#level-title").text("Game Over, Press Any Key to Restart");
     startOver();
   }
 }
 
+//Resets the game
 function startOver() {
   level = 0;
   gamePattern = [];
-  started = false;
+  gameStarted = false;
 }
